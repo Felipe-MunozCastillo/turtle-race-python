b332a5c5060c5f088ccb6b4f298d5ddc32b98bba
Felipe Muñoz <fmunoz.acastillo@gmail.com>
2024-01-17 20:22:14 -0300

    commit inicial

---
+++ turtle_race.py
@@ -0,0 +1,113 @@
+from turtle import Turtle, Screen
+import random
+
+
+def define_colors(bet):
+    if bet == 'roja' or bet == 'rojo':
+        bet = "red"
+        return bet
+    elif bet == "amarilla" or bet == "amarillo":
+        bet = "yellow"
+        return bet
+    elif bet == "naranja" or bet == "naranjo":
+        bet = "orange"
+        return bet
+    elif bet == "verde":
+        bet = "green"
+        return bet
+    elif bet == "azul":
+        bet = "blue"
+        return bet
+    elif bet == "purpura":
+        bet = "purple"
+        return bet
+    else:
+        return False
+
+
+def create_turtles(bet, turtle_colors):
+
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(user_bet)
+    tim.goto(x=-230, y=-100)
+    all_turtles.append(tim)
+
+    turtle_colors.remove(bet)
+
+    for iteration in range(len(turtle_colors)):
+        create_a_turtle(-230, -100, iteration, colors[iteration])
+
+
+def create_a_turtle(position_x, position_y, iteration, color):
+    turtle = Turtle(shape="turtle")
+    turtle.color(color)
+    turtle.penup()
+    turtle.goto(position_x, position_y + (iteration + 1) * 50)
+    all_turtles.append(turtle)
+
+
+def reverse_colors(bet):
+    if bet == 'red':
+        bet = "roja"
+        return bet
+    elif bet == "yellow":
+        bet = "amarilla"
+        return bet
+    elif bet == "orange":
+        bet = "naranja"
+        return bet
+    elif bet == "green":
+        bet = "verde"
+        return bet
+    elif bet == "blue":
+        bet = "azul"
+        return bet
+    elif bet == "purple":
+        bet = "purpura"
+        return bet
+    else:
+        return False
+
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+
+user_bet = False
+user_bet = screen.textinput(
+    title="Has tu apuesta", prompt="¿Que tortuga ganará la carrera? Ingresa un color: ").lower()
+user_bet = define_colors(user_bet)
+
+while not user_bet:
+    user_bet = screen.textinput(title="Has tu apuesta",
+                                prompt="Lo siento, no tenemos de esas. escoge otro color: ").lower()
+    user_bet = define_colors(user_bet)
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = []
+
+create_turtles(user_bet, colors)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                winning_color = reverse_colors(winning_color)
+                print(f"¡Ganaste! La tortuga {winning_color} es la ganadora.")
+            else:
+                winning_color = reverse_colors(winning_color)
+                print(f"¡Perdiste! La tortuga {winning_color} es la ganadora.")
+        else:
+            random_distance = random.randint(0, 10)
+            turtle.forward(random_distance)
+
+
+screen.exitonclick()
